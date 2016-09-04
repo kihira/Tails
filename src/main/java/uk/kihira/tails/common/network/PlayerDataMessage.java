8120781f15b90c0199a1a470938b6f59e279a015
Kihira <kihira@kihirakreations.co.uk>
2016-09-04 21:40:50 +0100

    Fix logic in PlayerDataMesssage to allow proper deleting with null data

--- src/main/java/uk/kihira/tails/common/network/PlayerDataMessage.java
+++ src/main/java/uk/kihira/tails/common/network/PlayerDataMessage.java
@@ -8,15 +8,16 @@
 
 package uk.kihira.tails.common.network;
 
+import com.google.common.base.Strings;
 import com.google.gson.JsonSyntaxException;
 import com.mojang.util.UUIDTypeAdapter;
 import io.netty.buffer.ByteBuf;
-import uk.kihira.tails.common.PartsData;
-import uk.kihira.tails.common.Tails;
 import net.minecraftforge.fml.common.network.ByteBufUtils;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
+import uk.kihira.tails.common.PartsData;
+import uk.kihira.tails.common.Tails;
 
 import java.util.UUID;
 
@@ -37,17 +38,20 @@ public class PlayerDataMessage implements IMessage {
     public void fromBytes(ByteBuf buf) {
         uuid = UUIDTypeAdapter.fromString(ByteBufUtils.readUTF8String(buf));
         String tailInfoJson = ByteBufUtils.readUTF8String(buf);
-        try {
-            partsData = Tails.gson.fromJson(tailInfoJson, PartsData.class);
-        } catch (JsonSyntaxException e) {
-            Tails.logger.warn(e);
+        if (!Strings.isNullOrEmpty(tailInfoJson)) {
+            try {
+                partsData = Tails.gson.fromJson(tailInfoJson, PartsData.class);
+            } catch (JsonSyntaxException e) {
+                Tails.logger.warn(e);
+            }
         }
+        else partsData = null;
     }
 
     @Override
     public void toBytes(ByteBuf buf) {
         ByteBufUtils.writeUTF8String(buf, UUIDTypeAdapter.fromUUID(uuid));
-        String tailInfoJson = Tails.gson.toJson(this.partsData);
+        String tailInfoJson = partsData == null ? "" : Tails.gson.toJson(this.partsData);
         ByteBufUtils.writeUTF8String(buf, tailInfoJson);
     }
 
@@ -55,14 +59,12 @@ public class PlayerDataMessage implements IMessage {
 
         @Override
         public IMessage onMessage(PlayerDataMessage message, MessageContext ctx) {
-            if (message.partsData != null) {
-                if (message.shouldRemove) Tails.proxy.removePartsData(message.uuid);
-                else {
-                    Tails.proxy.addPartsData(message.uuid, message.partsData);
-                    //Tell other clients about the change
-                    if (ctx.side.isServer()) {
-                        Tails.networkWrapper.sendToAll(new PlayerDataMessage(message.uuid, message.partsData, false));
-                    }
+            if (message.shouldRemove) Tails.proxy.removePartsData(message.uuid);
+            else if (message.partsData != null) {
+                Tails.proxy.addPartsData(message.uuid, message.partsData);
+                //Tell other clients about the change
+                if (ctx.side.isServer()) {
+                    Tails.networkWrapper.sendToAll(new PlayerDataMessage(message.uuid, message.partsData, false));
                 }
             }
             return null;
