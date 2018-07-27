23a63322565604129a97af6fdfc186c64cfb0cfb
Zoe <zoe.kihira.lee@gmail.com>
2018-07-27 09:35:19 +0200

    Create threetint shader that should use the one mega texture

--- src/main/resources/assets/tails/shader/threetint_frag.glsl
+++ src/main/resources/assets/tails/shader/threetint_frag.glsl
@@ -1,9 +1,27 @@
-#version 120
+#version 130
 
 in vec2 TexCoord;
 
 uniform sampler2D tex;
+uniform vec3[3] tints;
 
 void main() {
-  gl_FragColor = texture(tex, TexCoord);
+  vec4 texCol = texture(tex, TexCoord);
+/*
+  Experimental version using alpha as the tone instead of red channel
+  float tone = texCol.a;
+  float red = tints[0].r * texCol.r + tints[1].r * texCol.g + tints[2].r * texCol.b;
+  float green = tints[0].g * texCol.r + tints[1].g * texCol.g + tints[2].g * texCol.b;
+  float blue = tints[0].b * texCol.r + tints[1].b * texCol.g + tints[2].b * texCol.b;
+  */
+
+  float tone = texCol.r;
+  float w1 = 1.0 - (texCol.g + texCol.b);
+  float w2 = texCol.g * (1.0 - texCol.b);
+  float w3 = texCol.b;
+  float red = tints[0].r * w1 + tints[1].r * w2 + tints[2].r * w3;
+  float green = tints[0].g * w1 + tints[1].g * w2 + tints[2].g * w3;
+  float blue = tints[0].b * w1 + tints[1].b * w2 + tints[2].b * w3;
+
+  gl_FragColor = vec4(tone * red, tone * green, tone * blue, texCol.a);
 }
--- src/main/resources/assets/tails/shader/threetint_vert.glsl
+++ src/main/resources/assets/tails/shader/threetint_vert.glsl
@@ -1,4 +1,4 @@
-#version 120
+#version 130
 
 layout (location = 0) in vec3 pos;
 layout (location = 1) in vec3 normal;
