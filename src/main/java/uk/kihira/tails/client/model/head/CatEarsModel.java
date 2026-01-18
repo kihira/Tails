b1f902d58180bd0ee2f77f8c87b85573de60aca7
kihira <zoe@kihira.uk>
2026-01-18 12:13:47 +0000

    Fixing up models

--- src/main/java/uk/kihira/tails/client/model/head/CatEarsModel.java
+++ src/main/java/uk/kihira/tails/client/model/head/CatEarsModel.java
@@ -27,27 +27,27 @@ public final class CatEarsModel extends PartModel
 
 		var leftEar = partRoot.addOrReplaceChild("leftEar",
 				CubeListBuilder.create()
-						.texOffs(0, 0).addBox(3.0F, -9.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 16).addBox(3.0F, -12.0F, 1.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 14).addBox(3.0F, -11.0F, 1.0F, 2.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 12).addBox(3.0F, -10.0F, 1.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 2).addBox(2.0F, -10.0F, 0.0F, 5.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 8).addBox(3.0F, -13.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 6).addBox(2.0F, -12.0F, 0.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(0, 4).addBox(2.0F, -11.0F, 0.0F, 4.0F, 1.0F, 1.0F, CubeDeformation.NONE),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
+						.texOffs(0, 0).addBox(-5.0F, -8.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 16).addBox(-5.0F, -11.0F, 1.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 14).addBox(-5.0F, -10.0F, 1.0F, 2.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 12).addBox(-5.0F, -9.0F, 1.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 2).addBox(-6.0F, -9.0F, 0.0F, 5.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 8).addBox(-5.0F, -12.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 6).addBox(-6.0F, -11.0F, 0.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(0, 4).addBox(-6.0F, -10.0F, 0.0F, 4.0F, 1.0F, 1.0F, CubeDeformation.NONE),
+				PartPose.ZERO);
 
 		var rightEar = partRoot.addOrReplaceChild("rightEar",
 				CubeListBuilder.create()
-						.texOffs(13, 0).addBox(-4.0F, -9.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 16).addBox(-4.0F, -12.0F, 1.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 14).addBox(-5.0F, -11.0F, 1.0F, 2.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 12).addBox(-6.0F, -10.0F, 1.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 2).addBox(-7.0F, -10.0F, 0.0F, 5.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 8).addBox(-4.0F, -13.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 6).addBox(-5.0F, -12.0F, 0.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
-						.texOffs(13, 4).addBox(-6.0F, -11.0F, 0.0F, 4.0F, 1.0F, 1.0F, CubeDeformation.NONE),
-				PartPose.offset(0.0F, 0.0F, 0.0F));
+						.texOffs(13, 0).addBox(4.0F, -8.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 16).addBox(4.0F, -11.0F, 1.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 14).addBox(3.0F, -10.0F, 1.0F, 2.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 12).addBox(2.0F, -9.0F, 1.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 2).addBox(1.0F, -9.0F, 0.0F, 5.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 8).addBox(4.0F, -12.0F, 0.0F, 1.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 6).addBox(3.0F, -11.0F, 0.0F, 3.0F, 1.0F, 1.0F, CubeDeformation.NONE)
+						.texOffs(13, 4).addBox(2.0F, -10.0F, 0.0F, 4.0F, 1.0F, 1.0F, CubeDeformation.NONE),
+				PartPose.ZERO);
 
 		return LayerDefinition.create(meshDefinition, 64, 32);
 	}
